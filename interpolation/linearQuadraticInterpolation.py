456e665ebc7832e373e771123c17e8a1f2df173e
aLieexe <rizky.alay@gmail.com>
2024-12-10 12:39:55 +0700

    a little change in interpolation

--- interpolation/linearQuadraticInterpolation.py
+++ interpolation/linearQuadraticInterpolation.py
@@ -27,18 +27,34 @@ def quadratic_interpolation(x, y, x_new):
     return np.array(y_new)
 
 
-x = np.array([1, 2, 4, 5, 10, 15, 50])
+x = np.array([1, 2, 4, 5, 10, 25, 50])
 y = np.array([0, 2, 6, 8, 19, 30, 150])
 
-x_new = np.linspace(min(x), max(x), 100)
-y_linear = linear_interpolation(x, y, x_new)
-y_quadratic = quadratic_interpolation(x, y, x_new)
+x_graph = np.linspace(min(x), max(x), 100)
+y_linear_graph = linear_interpolation(x, y, x_graph)
+y_quadratic_graph = quadratic_interpolation(x, y, x_graph)
 
+x_preds = np.array([30]) 
+y_pred_linear = linear_interpolation(x, y, x_preds)
+y_pred_quadratic = quadratic_interpolation(x, y, x_preds)
+
+print("Menggunakan Interpolasi linear: ")
+for xi, yi in zip(x_preds, y_pred_linear):
+    print(f"Prediksi y untuk x = {xi} adalah y = {yi}")
+
+print("Menggunakan Interpolasi kuadratik: ")
+for xi, yi in zip(x_preds, y_pred_quadratic):
+    print(f"Prediksi y untuk x = {xi} adalah y = {yi}")
 
 plt.figure(figsize=(10, 6))
 plt.plot(x, y, 'o', label='Data points', markersize=8)
-plt.plot(x_new, y_linear, '-', label='Linear Interpolation')
-plt.plot(x_new, y_quadratic, '-', label='Quadratic Interpolation')
+plt.plot(x_graph, y_linear_graph, '-', label='Linear Interpolation')
+plt.plot(x_graph, y_quadratic_graph, '-', label='Quadratic Interpolation')
+
+plt.plot(x_preds, y_pred_linear, 'ro', label='Predicted points (Linear)')
+plt.plot(x_preds, y_pred_quadratic, 'ro', label='Predicted points (Quadratic)')
+
+
 plt.legend()
 plt.xlabel('x')
 plt.ylabel('y')
